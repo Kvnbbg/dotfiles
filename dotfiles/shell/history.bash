ef9c2edb4f1694365a0c3713d2135e67b6064145
Kevin Marville <kvnbbg@gmail.com>
2025-08-25 03:05:30 +0200

    add dotfiles from local to remote

---
+++ dotfiles/shell/history.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Bash history control
+HISTFILE="${HOME}/.shell/history/bash"
+HISTSIZE=20000
+HISTFILESIZE=20000
+HISTCONTROL=ignoreboth:erasedups
---
+++ dotfiles/shell/prompt.bash
@@ -0,0 +1,39 @@
+RED="\e[1;31m"
+GREEN="\e[1;32m"
+OFF="\e[0m"
+
+function __exit_status {
+	local EXITSTATUS="${?}"
+	if [[ "${EXITSTATUS}" -ne "0" ]]; then
+		echo -e "|${RED}${EXITSTATUS}${OFF}\$ "
+	fi
+}
+
+function __git_ps1 {
+	local b
+	b="$(git symbolic-ref HEAD 2>/dev/null)";
+	if [[ -n "${b}" ]]; then
+		printf "%s" "${b##refs/heads/}";
+	fi
+}
+
+function __git_status {
+	local status
+	status="$(__git_ps1 %s)"
+	if [[ -n "${status}" ]]; then
+		echo -e "${GREEN}@${status}${OFF}"
+	fi
+}
+
+# Start from scratch
+PS1=""
+
+if [[ -n "${SSH_CLIENT}" ]]; then
+	PS1='\u@\H:'
+fi
+
+if [[ -f "/run/.toolboxenv" ]] || [[ -f "/run/.containerenv" ]]; then
+	PS1="📦${PS1}"
+fi
+
+PS1=${PS1}'$(__git_status)$(__exit_status)\$ '
